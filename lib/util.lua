af2b64ee6b4a3ba200debca95e3aeba4d97f83c6
Shanethe13 <shane.wilton@gmail.com>
2011-04-27 00:43:25 -0400

    Created a vector class

--- lib/util.lua
+++ lib/util.lua
@@ -16,3 +16,33 @@ end
 function Rectangle:center()
 	return {math.floor((self.x1 + self.x2) / 2), math.floor((self.y1 + self.y2) / 2)}
 end
+
+Vector2d = class("Vector2d")
+
+function Vector2d:initialize(x, y)
+	self.x = x
+	self.y = y
+end
+
+function Vector2d:add(other)
+	return Vector2d:new(self.x + other.x, self.y + other.y)
+end
+
+function Vector2d:scale(scalar)
+	return Vector2d:new(self.x*scalar, self.y*scalar)
+end
+
+function Vector2d:length()
+	return math.sqrt(self.x*self.x + self.y*self.y)
+end
+
+--Return the sum of the squared components.  Faster than length since it ignores the square root
+function Vector2d:length2()
+	return self.x*self.x + self.y*self.y
+end
+
+function Vector2d:normalize()
+	local length = self:length()
+	if length == 0 then return self
+	else return self:scale(1/length) end
+end
